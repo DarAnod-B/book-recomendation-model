7cfa900a33a24816836b35d0945d75ddc397c19e
David Butaev <79657855+DarAnod-B@users.noreply.github.com>
2023-01-03 15:24:17 +0300

    Создание метода main

--- model/main.py
+++ model/main.py
@@ -1,15 +1,23 @@
 import pandas as pd
 from UserItem_model import UserBasedRecommendation
 
-df = pd.read_csv(
-    r"C:\Programming\GitHub\book-recomendation-system\data\data.csv", index_col=0)
 
-train = df.query("user != 1")
-test = df.query("user == 1")
+def main():
+    target_user_id = 1
 
-model = UserBasedRecommendation()
-model.fit(train, user_id_col_name='user',
-          item_id_col_name='id_book', grade_col_name='grade')
+    df = pd.read_csv(
+        r"C:\Programming\GitHub\book-recomendation-system\data\data.csv", index_col=0)
 
-score = model.predict(test)
-print(score)
+    train = df.query("user != @target_user_id")
+    test = df.query("user == @target_user_id")
+
+    model = UserBasedRecommendation()
+    model.fit(train, user_id_col_name='user',
+              item_id_col_name='id_book', grade_col_name='grade')
+
+    score = model.predict(test)
+    print(score)
+
+
+if __name__ == "__main__":
+    main()
